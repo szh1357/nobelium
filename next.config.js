4f1e3e3bf1e8b9358662839606742d4a6744609c
szh1357 <shanegreen168@gmail.com>
2025-11-20 21:39:09 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   staticPageGenerationTimeout: 300,
   images: {
-    domains: ['www.notion.so', 'notion.so', 'images.unsplash.com', 'pbs.twimg.com', 'abs.twimg.com', 's3.us-west-2.amazonaws.com', 'transitivebullsh.it'],
+    domains: [
+      'www.notion.so',
+      'notion.so',
+      'images.unsplash.com',
+      'pbs.twimg.com',
+      'abs.twimg.com',
+      's3.us-west-2.amazonaws.com',
+      'transitivebullsh.it'
+    ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  }
+  },
 }
+
+module.exports = nextConfig
